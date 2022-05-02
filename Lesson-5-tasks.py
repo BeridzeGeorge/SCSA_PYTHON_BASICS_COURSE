4cab496357c4c5442322e9e15897e6b4b815a79b
Giorgi Beridze <gioberidze@gmail.com>
2022-05-02 15:44:56 +0400

    All tasks finished.

--- Lesson-5-tasks.py
+++ Lesson-5-tasks.py
@@ -22,3 +22,18 @@ while True:
         print("sum of numbers from 1 to 20 are: ",sum)
         print("Program ended...")
         break
+#Lesson 5 task 2
+"""
+2. პროგრამამ მოგვთხოვოს სახელის შეყვანა, სანამ შეყვანილი სახელის სიგრძე ნაკლები იქნება 2-ზე, დაწეროს"2 ასო მაინც შეიყვანე" და ისევ მოგვთხოვოს სახელის შეყვანა, ასევე, სანამ სახელის სიგრძე მეტია 10-ზე პროგრამამ დაწეროს "ცოტა მოკლედ დაწერე" და ისევ მოგვთხოვოს სახელის შეყვანა, თუ სახელის სიგრძე ეტევა 2-დან 8-მდე დაბეჭდოს ეს სიტყვა და დაასრულოს მუშაობა."""
+name = input('Enter the name')
+while True:
+
+	if len(name) < 2:
+		print("Name should contains at least 2 characters")
+		name = input("Try again: ")
+	elif len(name) > 10:
+		print("Too long")
+		name = input("Try again with shorter one: ")
+	else:
+		print(name)
+		break
