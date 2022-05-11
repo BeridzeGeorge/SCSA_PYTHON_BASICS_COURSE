42bb317408c1714adb4c0e6c73f8f80442d1c270
Giorgi Beridze <gioberidze@gmail.com>
2022-05-11 10:43:39 +0400

    Lesson 8, tasks 1 & 2 finished.

--- Lesson-8-tasks-1-2.py
+++ Lesson-8-tasks-1-2.py
@@ -3,6 +3,7 @@
 # Task №1
 """დაწერეთ ფუნქცია, რომელიც პარამეტრად იღებს ლისტს და გადაყავს ეს ლისტი ტუპლში"""
 
+
 def list_to_tuple(x):
     # list to tuple converter, using for loop
     tpl = ()
@@ -10,11 +11,14 @@ def list_to_tuple(x):
         tpl += (i,)
     return tpl
 
+
 print(list_to_tuple([1, 2, "a"]))
+
 
 def list_to_tuple_converter(x):
     # list to tuple converter, using method
     return tuple(x)
+
 
 print(list_to_tuple_converter([3, 4, "b"]))
 
@@ -22,16 +26,21 @@ print(list_to_tuple_converter([3, 4, "b"]))
 """ დაწერეთ ფუნქცია, რომელიც პარამეტრად იღებს ტუპლს და გადაყავს ეს ტუპლი ლისტში
 """
 
+
 def tuple_to_list(x):
     # tuple to list converter, using method
     return list(x)
 
+
 print(tuple_to_list((5, 6, "c")))
 
-def tuple_to_list_converter (x):
+
+def tuple_to_list_converter(x):
     # tuple to list converter, using for loop
     lst = []
     for i in x:
         lst.append(i)
     return lst
-print(tuple_to_list_converter((7,8,"d")))
+
+
+print(tuple_to_list_converter((7, 8, "d")))
