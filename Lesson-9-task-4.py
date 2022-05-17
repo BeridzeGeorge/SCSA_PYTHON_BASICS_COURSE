f138ce095e9bd4035cfce8a0eeb7e69fda571cd4
Giorgi Beridze <gioberidze@gmail.com>
2022-05-17 12:07:22 +0400

    Lesson 9 task 4 finished.

---
+++ Lesson-9-task-4.py
@@ -0,0 +1,24 @@
+# ---------- GIORGI BERIDZE ----------
+# Lesson 9 home tasks
+# Task №4
+"""4. გვაქვს ლისტი, a=["giorgi", "pavle", "mamuka", "dachi","irakli","iago",
+"natia", "nino","nika","tamazi" ]პროგრამამ უნდა გადაწეროს ამ ლისტის ელემენტები
+დიქშენერში, კეის სახით, ხოლო ველიუები გმოიმუშავოს რენდომულად და მიანიჭოს ქულების
+ სახით თითოეულ ელემენტს.
+4.1 გავაკეთოტ იგივე ფუნქციით, სადაც პარამეტრად გადავცემთ ლისტს,
+ხოლო ველიუებს გამოიმუშავებს ავტომატურად ფუნქციის შიგნით."""
+
+import random
+
+a = ["giorgi", "pavle", "mamuka", "dachi", "irakli", "iago",
+     "natia", "nino", "nika", "tamazi"]
+
+
+def students_points(x_list):
+    dic = {}
+    for i in x_list:
+        dic[i] = random.randint(40, 100)
+    return dic
+
+
+print(students_points(a))
